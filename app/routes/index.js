f7cee18a86dc1b76a223ed95a4eb9e8acfd80c4e
antonbavykin1991 <antonbavykin1991@gmail.com>
2017-09-16 02:39:26 +0300

    testing fastboot

--- app/routes/index.js
+++ app/routes/index.js
@@ -1,4 +1,11 @@
 import Ember from 'ember';
+import fetch from 'ember-fetch/ajax';
 
 export default Ember.Route.extend({
+  model() {
+    return fetch('https://api.github.com/users/tomdale')
+      .then(function(response) {
+        return response;
+      });
+  }
 });
