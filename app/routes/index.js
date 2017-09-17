69e9f20a071dad110293d7e30b1949cb03ed312d
antonbavykin1991 <antonbavykin1991@gmail.com>
2017-09-17 18:53:13 +0300

    testing styles

--- app/routes/index.js
+++ app/routes/index.js
@@ -2,16 +2,7 @@ import Ember from 'ember';
 import RSVP from 'rsvp';
 
 export default Ember.Route.extend({
-  fastboot: Ember.inject.service(),
-
-  isFastBoot: Ember.computed.reads('fastboot.isFastBoot'),
-
   model() {
-    if (this.get('isFastBoot')) {
-      let headers = this.get('fastboot.request.headers');
-      console.log(headers);
-    }
-
     return RSVP.hash({
       users: this.store.findAll('user')
     })
